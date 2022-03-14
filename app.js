1739d3a8d8252afee40f44fbd7721da0af836732
ertugrulocal17 <ertugrulocal17@gmail.com>
2022-03-14 20:22:20 +0300

    added mongoose and post operation assgin-9

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
 const express = require('express');
 const app = express();
 const ejs = require('ejs');
-
+const mongoose = require('mongoose');
+const Post = require('./models/Post');
 // MIDDLEWARE
 app.use(express.static('public'));
-
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 //TEMPLATE ENGINE
 app.set('view engine', 'ejs');
-app.get('/', (req, res) => {
-  res.render('index');
+
+// CONNECT DB
+mongoose.connect('mongodb://localhost/cleanblog-test-db');
+
+app.get('/', async (req, res) => {
+  const posts = await Post.find({});
+  res.render('index', {
+    posts: posts,
+  });
+});
+
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
+
+app.post('/posts', async (req, res) => {
+  await Post.create(req.body);
+  res.redirect('/');
 });
 
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}`);
+  console.log(`Server is running port ${PORT}`);
 });
